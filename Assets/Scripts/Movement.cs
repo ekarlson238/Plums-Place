83ec0320cdb289469b2c677d12760a1db3d28314
ekarlson238 <ekarlson238@gmail.com>
2018-09-19 14:27:05 -0500

    Added floor and player w/ horiz movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float spd;
+    public float xAxis;
+    private Rigidbody2D rb;
+
+    public float jumpForce;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        xAxis = Input.GetAxisRaw("Horizontal") * spd;
+
+        //Move with X = Left right
+        Vector2 VelocityX = rb.velocity;
+        VelocityX.x = xAxis;
+        rb.velocity = VelocityX;
+
+        //Max Movement spd
+        if (rb.velocity.magnitude > spd)
+        {
+            rb.velocity *= spd / rb.velocity.magnitude;
+        }
+
+        
+
+    }
+}
