5eeb0e7aaa55c38007062930dd6434440e9f6fcb
ekarlson238 <ekarlson238@gmail.com>
2018-09-26 14:11:41 -0500

    Fixed jumping and added a dash
    added grounded to only allow 1 jump
    added a dash that can only be done once while jumping

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,33 +4,67 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    public float spd;
-    public float xAxis;
+    [SerializeField]
+    private float spd;
+
+    private float xAxis;
     private Rigidbody2D rb;
 
-    public float jumpHeight;
+    [SerializeField]
+    private float jumpHeight;
+
+    private bool grounded;
+    private bool canDash;
+
+    [SerializeField]
+    private float dashMult;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        grounded = true;
+        canDash = false;
+    }
+
+    //if the player collides with something tagged ground
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true; //let me jump again
+            canDash = false; //can't dash if grounded
+        }
     }
 
     void FixedUpdate()
     {
-        //Movement
+        //value for x velocity
         xAxis = Input.GetAxisRaw("Horizontal") * spd;
 
-        //Move with X = Left right
+        //set object's x velocity to xAxis
         Vector2 VelocityX = rb.velocity;
         VelocityX.x = xAxis;
-        rb.velocity = VelocityX;
         
-
         //jump
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            rb.AddForce(Vector2.up * jumpHeight);
+            VelocityX.y = jumpHeight; //sets y velocity
+            grounded = false;//test to see if already jumping
+            canDash = true;//can only dash while jumping
         }
 
-    }
+        //press shift to dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            VelocityX.x = xAxis * dashMult;
+
+            canDash = false; //can only dash once per jump
+        }
+
+        rb.velocity = VelocityX; //sets velocity
+
+    }//fixedUpdate
+
+    
+
 }
