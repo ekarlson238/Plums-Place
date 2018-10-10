20b1e1e9ca1cadcb639897fdf8e33927e2237b24
ekarlson238 <ekarlson238@gmail.com>
2018-10-10 14:56:32 -0500

    Created coin prefab

---
+++ Assets/Scripts/ifDestroyed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ifDestroyed : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject self;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (self == null)
+        {
+            //reset scene
+
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
