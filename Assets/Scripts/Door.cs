34604e2dea0f7dd135b16b31fc629cf2071431cd
ekarlson238 <ekarlson238@gmail.com>
2018-10-24 15:11:46 -0500

    Door code working and door prefab made

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField]
+    private string nextScene;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+
+    }
+}
