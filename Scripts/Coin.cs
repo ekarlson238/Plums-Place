732d09c1de6497520f780577270fa45179dcfe12
ekarlson238 <ekarlson238@gmail.com>
2018-11-21 13:44:37 -0600

    re-added scripts for code review

---
+++ Scripts/Coin.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    private static int coinCount_UseProperty;
+    private CoinUI coinUI;
+    private bool isCollected;
+    public static int CoinCount
+    {
+        set
+        {
+            if (value >= 0)
+                coinCount_UseProperty = value;
+
+            
+        }
+        get
+        {
+            return coinCount_UseProperty;
+        }
+    }
+
+    private void Awake()
+    {
+        coinUI = GameObject.Find("CoinsCollectedUI").GetComponent<CoinUI>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") //if touches coin
+        {
+            if (!isCollected)
+            {
+                isCollected = true;
+                CoinCount++;
+                coinUI.UpdateScoreText();
+            }
+            
+
+            /*
+            Destroy(collision.gameObject); //destroy the coin
+            coinsCollected += 1; //add one to coins collected
+            coinText.text = "Coins: " + coinsCollected + "/3";
+            */
+
+            /*
+            if(collision.gameObject != null)
+            {
+                coinsCollected += 1; //add one to coins collected
+                coinText.text = "Coins: " + coinsCollected + "/3";
+                Destroy(collision.gameObject); //destroy the coin
+            }
+            */
+
+
+            gameObject.SetActive(false);
+            //coinsCollected += 1; //add one to coins collected
+            //coinText.text = "Coins: " + coinsCollected + "/3";
+
+        }
+    }
+}
---
+++ Scripts/Movement.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    private float speedSave;
+    private float halfSpeed;
+
+    private float xAxis;
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float jumpHeight;
+
+    private bool grounded;
+    private bool canDash;
+    private bool dashing;
+
+    [SerializeField]
+    private float dashsSpeedMultiplier;
+
+    private float dashVelocity;
+
+    [SerializeField]
+    private float dashDurationSave;
+    private float dashDuration;
+    
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private GameObject playerSprite;
+    private Vector3 originalScale;
+
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+    private Vector2 velocity;
+
+    private float horizontalRawAxisValue;
+
+    [SerializeField]
+    private GameObject shield;
+
+    private Checkpoint currentCheckpoint;
+
+    [SerializeField]
+    private Button deathButton;
+
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        grounded = true;
+        canDash = false;
+        dashing = false;
+        dashDuration = dashDurationSave;
+        originalScale = playerSprite.transform.localScale;
+
+        velocity = rb.velocity;
+        speedSave = speed;
+        halfSpeed = speed / 2;
+
+        horizontalRawAxisValue = 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "dashThrough" && !dashing) //if hits laser without dashing
+        {
+            Death();
+        }
+
+        if (collision.gameObject.tag == "Water") //if touches water
+        {
+            Death();
+        }
+        
+    }
+
+    private void Death()
+    {
+        deathButton.onClick.AddListener(DeathButtonPressed);
+        deathButton.gameObject.SetActive(true);
+        Time.timeScale = 0;
+        
+    }
+
+    private void DeathButtonPressed()
+    {
+        deathButton.gameObject.SetActive(false);
+
+        if (currentCheckpoint == null)
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            Respawn();
+        }
+
+        Time.timeScale = 1;
+
+    }
+
+    void FixedUpdate()
+    {
+        UpdateGrounded();
+
+        SetHorizontalVelocity();
+
+        Jump();
+
+        Dash();
+
+        DashShield();
+
+        MovementDependingOnIfDashing();
+
+    }//fixedUpdate
+
+    private void DashShield()
+    {
+        if (dashing)
+        {
+            shield.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            shield.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    private void UpdateGrounded()
+    {
+        grounded = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+        //Debug.Log("grounded =" + grounded);
+
+        if (grounded)
+        {
+            canDash = true; //landing on ground resets dash
+
+            animator.SetBool("isJumping", false);
+            speed = speedSave;
+        }
+        else
+        {
+            speed = halfSpeed;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    private void MovementDependingOnIfDashing()
+    {
+        if (!dashing)//if you're not dashing
+        {
+            rb.velocity = velocity; //sets velocity
+            animator.SetFloat("Speed", Mathf.Abs(velocity.x));
+
+            MirrorPlayerSprite();
+
+        }
+        else//if you are dashing, continue to dash for dashDuration, then stop dashing
+        {
+            dashDuration -= Time.deltaTime;
+            if (dashDuration < 0)
+            {
+                dashing = false;
+                dashDuration = dashDurationSave;
+                UnfreezeY();
+
+                animator.SetBool("isDashing", false);
+            }
+        }
+    }
+
+    private void MirrorPlayerSprite()
+    {
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerSprite.transform.localScale = new Vector3(-(originalScale.x), originalScale.y, originalScale.z);
+            horizontalRawAxisValue = Input.GetAxisRaw("Horizontal");
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerSprite.transform.localScale = originalScale;
+            horizontalRawAxisValue = Input.GetAxisRaw("Horizontal");
+        }
+    }
+
+    private void Dash()
+    {
+        //press shift to dash if you haven't dashed before, you are not grounded, and you are moving in a horizonal direction
+        if (Input.GetButton("Fire3") && canDash && !grounded)
+        {
+            dashVelocity = horizontalRawAxisValue * speedSave * dashsSpeedMultiplier;
+
+            FreezeY(); //freeze y position while dashing
+            rb.AddForce(new Vector2(dashVelocity, 0));
+            dashing = true;
+
+            canDash = false; //can only dash once per jump
+
+            animator.SetBool("isDashing", true);
+        }
+    }
+
+    private void Jump()
+    {
+        //jump
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpHeight; //sets y velocity
+        }
+    }
+
+    private void SetHorizontalVelocity()
+    {
+        //value for x velocity
+        xAxis = Input.GetAxisRaw("Horizontal") * speed;
+
+        //set object's x velocity to xAxis
+        velocity = rb.velocity;
+        velocity.x = xAxis;
+    }
+
+    private void FreezeY()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation| RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    private void UnfreezeY()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    public void SetCurrentCheckpoint(Checkpoint newCurrentCheckpoint)
+    {
+        currentCheckpoint = newCurrentCheckpoint;
+    }
+
+    private void Respawn()
+    {
+        rb.velocity = new Vector2(0, 0);
+        transform.position = currentCheckpoint.transform.position;
+    }
+
+
+}
